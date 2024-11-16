7b81e10d6491072f7ac49b7f620cddf32ac7e887
AlexPC <alexarrieg@gmail.com>
2024-11-16 17:01:28 +0100

    comments

--- yolo_inference.py
+++ yolo_inference.py
@@ -5,6 +5,8 @@ from ultralytics import YOLO
 # tried it on M1 macbook air and working fine
 model = YOLO("yolov8x")
 
+# This command will run the model to detect objects in the image
+# or video selected, and save the output into the 'runs/detect/predict' folder
 results = model.predict("input_videos/08fd33_4.mp4", save=True)
 print(results[0])
 print("######################################")
